e5638c7d16a575362e06b195c76a7f2418e9f862
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-03-08 17:43:53 +0200

    array helper binary operations

--- src/helpers/array-helper.js
+++ src/helpers/array-helper.js
@@ -16,5 +16,35 @@ export default class ArrayHelper {
         return [...new Set([].concat(...args))];
     }
 
+    static addSortedArray(el, arr,  compare  = (a,b) => a - b) {
+
+        arr.splice( ArrayHelper.binarySearch(el, arr, 0, arr.length, compare ) + 1, 0, el);
+
+        return arr;
+    }
+
+    static removeSortedArray(el, arr, compare  = (a,b) => a - b) {
+
+        const found = ArrayHelper.binarySearch(el, arr, 0, arr.length, compare);
+        if (arr[found] === el)
+            arr.splice( found, 1);
+
+        return arr;
+    }
+
+    static binarySearch(el, arr, st, en, compare) {
+
+        const pivot = (st + en) >> 1;  // should be faster than dividing by 2
+
+        const c = compare(el, arr[pivot]);
+
+        if (en - st <= 1 ) return c < 0 ? pivot - 1 : pivot;
+
+        if (c < 0) return ArrayHelper.binarySearch(el, arr, st, pivot, compare);
+        if (c == 0) return pivot;
+        if (c > 0) return ArrayHelper.binarySearch(el, arr, pivot, en, compare);
+
+    }
+
 }
 
